35c3dfb2b4cce685dc3214a243d0a316b43b4332
NeuroSystem <michal.feliksik@gmail.com>
2017-04-08 11:37:09 +0200

    poprawka kodowania

---
+++ Workflow/UserData/UI/Html/Widgets/Panel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuroSystem.Workflow.UserData.UI.Html.Widgets
+{
+    public class Panel : WidgetBase
+    {
+    }
+}
