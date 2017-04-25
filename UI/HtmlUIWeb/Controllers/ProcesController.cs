fbf0b1501002f6c7e6cdbd1743ccbd5ea15262f7
neurosystemeu <michal.feliksik@gmail.com>
2017-04-25 17:45:28 +0200

    dodanie brakujących plików

---
+++ UI/HtmlUIWeb/Controllers/ProcesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+using NeuroSystem.Workflow.UserData.UI.Html.ASP.UI.Html.Version2.TestViews;
+using Kendo.Mvc.UI;
+
+namespace HtmlUIWeb.Controllers
+{
+    public class ProcesController : Controller
+    {
+        // GET: Proces
+        public ActionResult Index()
+        {
+            return View("Index", new ProcesMW(this));
+        }
+
+        public ActionResult PracownikTestModel_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            var lista = listaPracownikow();
+            var filtr = "k";
+            var result = lista.Where(p=>p.Nazwa.Contains(filtr)).ToDataSourceResult(request);
+            return Json(result, JsonRequestBehavior.AllowGet);
+            //using (var kontekst = UtworzKontekstWykonania())
+            //{
+            //    var projektId = PobierzParametrGuid(nameof(ZapytaniaOfertoweMW.ProjektId));
+            //    var jednostkaOrgId = PobierzParametrGuid(nameof(ZapytaniaOfertoweMW.JednostkaOrganizacyjnaId));
+            //    var logika = kontekst.Logika<FakturaBL>();
+            //    var zapytanie = logika.PobierzFaktury(projektId, jednostkaOrgId);
+            //    var result = zapytanie.ToDataSourceResult(request, o => new FakturaMW(o));
+            //    return Json(result);
+            //}
+            return null;
+        }
+
+
+        private List<PracownikTest> listaPracownikow()
+        {
+            var lista = new List<PracownikTest>();
+            var imiona = "Michał;Paweł;Łukasz;Jan;Tomek;Krzysiek;Robert;Kuba;Wojtek;Marcin".Split(';');
+            var nazwiska = "Kowalski;Nowak;Cich;Ciech".Split(';');
+
+            int i = 1;
+            foreach (var nazwisko in nazwiska)
+            {
+                foreach (var imie in imiona)
+                {
+                    lista.Add(new PracownikTest() { Nazwisko = nazwisko, Imie = imie, Id = i });
+                    i++;
+                }
+            }
+
+            return lista;
+        }
+    }
+}
---
+++ UI/HtmlUIWidgets/UI/Html/Mvc/Widgets/NsLabel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.UI;
+using Kendo.Mvc.Infrastructure;
+using NeuroSystem.Workflow.UserData.UI.Html.Mvc.UI;
+
+namespace NeuroSystem.Workflow.UserData.UI.Html.ASP.UI.Html.Mvc.Widgets
+{
+    public class NsLabel : Kendo.Mvc.UI.WidgetBase
+    {
+        public NsLabel(Label label, HtmlHelper helper) 
+            : base(helper.ViewContext, helper.ViewData)
+        {
+            this.label = label;
+        }
+
+        public NsLabel(Label label, HtmlHelper helper, IJavaScriptInitializer initializer) 
+            : base(helper.ViewContext, initializer, helper.ViewData)
+        {
+            this.label = label;
+        }
+
+        private Label label;        
+        public string Content { get; set; }
+
+        protected override void WriteHtml(HtmlTextWriter writer)
+        {
+            writer.WriteBeginTag("label");
+            foreach (var key in label.HtmlAttributes.Keys)
+            {
+                writer.WriteAttribute(key, label.HtmlAttributes[key].ToString());
+            }
+            writer.Write(">");
+
+            writer.Write(Content);
+
+            writer.WriteEndTag("label");
+        }
+    }
+}
